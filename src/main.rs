e285f728fa4d7b3a52d5d71213c08517d8278923
electroluxv2 <electroluxv2@gmail.com>
2022-07-06 17:23:28 +0200

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
-use std::future::Future;
 use std::process::Command;
-use std::time::Duration;
 use teloxide::prelude::*;
 use tokio::task;
-use tokio::time::sleep;
-
+use serde::Deserialize;
+use teloxide::types::InputFile;
+use url::Url;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
     log::info!("Starting throw dice bot...");
 
-    let bot = Bot::from_env().auto_send();
+    let bot = Bot::from_env().set_api_url(Url::parse("http://s2.budziszm.pl:8081").unwrap()).auto_send();
 
     teloxide::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
-        let text = message.text().unwrap();
+        let text = message.text().unwrap_or_else(|| "");
         if !text.starts_with("http") {
             return Ok(());
         }
@@ -27,14 +26,19 @@ async fn main() {
 
 async fn on_url(bot: AutoSend<Bot>, message: Message) -> Result<(), ()> {
 
-    spawn_ytdlp(message.text().unwrap());
+    let video_url = spawn_ytdlp(message.text().unwrap());
+    bot.send_message(message.chat.id, format!("Id: {video_url}"))
+        .await
+        .expect("Failed to send message");
 
-    bot.send_message(message.chat.id, "aaaaaaaaaaaaa").await.expect("Failed to send message");
+    bot.send_video(message.chat.id, InputFile::file(video_url))
+        .await
+        .expect("Failed to send video");
 
     Ok(())
 }
 
-fn spawn_ytdlp(url: &str) {
+fn spawn_ytdlp(url: &str) -> String {
     let output = Command::new("yt-dlp")
         .arg("-q")
         .arg("-S")
@@ -49,7 +53,27 @@ fn spawn_ytdlp(url: &str) {
         .output()
         .expect("Failed to run yt-dlp");
 
-    println!("status: {}", output.status);
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    let json = String::from_utf8_lossy(&output.stdout);
+
+    #[derive(Deserialize)]
+    struct Response {
+        id: String
+    }
+
+    let response: Response = serde_json::from_str(&json).unwrap();
+    format!("/var/www/html/v/{0}.mp4", response.id)
 }
+
+// enum Base {
+//     Number(f32),
+//     Boolean(bool)
+// }
+//
+// struct Params {
+//     arg: String,
+//     base: Base
+// }
+//
+// enum Filter {
+//     Bassboost(Params)
+// }
